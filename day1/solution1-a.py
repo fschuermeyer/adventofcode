c39c4f59a87dc870e4c769988242585396faafae
Felix Schürmeyer <fschuermeyer@gmail.com>
2020-12-02 00:56:53 +0100

    improvment

--- day1/solution1-a.py
+++ day1/solution1-a.py
@@ -5,15 +5,15 @@ from functools import partial
 # Solution with one Function
 
 with open('data/day1.txt') as file:
-    data = file.readlines()
+    data = [int(i) for i in file.readlines()] 
 
 def checkforSum(t,year):
     if reduce(lambda x,y: x+y,t) == year:
         return reduce(lambda x,y: x*y,t)
 
 # Erster Part
-print(list(filter(partial(is_not, None), [checkforSum([int(i),int(j)],2020) for i in data for j in data]))[0])
+print(list(filter(partial(is_not, None), [checkforSum([i,j],2020) for i in data for j in data]))[0])
 
 # Zweiter Part
-print(list(filter(partial(is_not, None), [checkforSum([int(i),int(j),int(z)],2020) for i in data for j in data for z in data]))[0])
+print(list(filter(partial(is_not, None), [checkforSum([i,j,z],2020) for i in data for j in data for z in data]))[0])
 
